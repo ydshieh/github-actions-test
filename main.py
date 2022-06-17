94474d525d8d34b1958304c977bb80477bc177d6
ydshieh <ydshieh@users.noreply.github.com>
2022-06-17 12:53:35 +0200

    "abcde"
    hello
    
    I am good

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharmsss33333')
+    print_hi('PyCharms777ss33333')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
